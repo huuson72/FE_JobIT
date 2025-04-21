3f923e969848d1b231d7c8d7e7197b1e5f5a1f9a
huuson72 <huuson0702@gmail.com>
2025-04-21 14:16:37 +0700

    upload deploy

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -5,16 +5,16 @@ export const getEnvironmentConfig = () => {
         // URL của frontend
         frontendUrl: isDevelopment 
             ? 'http://localhost:5173'  // URL local
-            : 'https://fe-jobit.onrender.com', // URL production
+            : 'https://hsjobit.onrender.com', // URL production
             
         // URL của backend
         backendUrl: isDevelopment
             ? 'http://localhost:8080'  // URL local
-            : 'https://be-jobit.onrender.com', // URL production
+            : 'https://hsjobit.onrender.com', // URL production
             
         // URL callback cho VNPay
         vnpayReturnUrl: isDevelopment
             ? 'http://localhost:5173/subscription/payment-result'  // URL local
-            : 'https://fe-jobit.onrender.com/subscription/payment-result' // URL production
+            : 'https://hsjobit.onrender.com/subscription/payment-result' // URL production
     };
 }; 
